1be2506bcf49c490fdc21924f3c8176b8ea4eddc
Pete Johnson <petecj2@gmail.com>
2012-07-17 16:42:53 -0700

    Initial Commit

---
+++ RockyDontEatThat/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  RockyDontEatThat
+//
+//  Created by Pete Johnson on 7/17/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
